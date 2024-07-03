8c5ffa6bb90a6280bcc0c39b09dfa1dd27ecb735
Hanae Messari Khali <hanaae.messari@gmail.com>
2024-07-03 22:40:36 +0100

    Update 07.Reverse Number.cpp

--- Level 2/07.Reverse Number.cpp
+++ Level 2/07.Reverse Number.cpp
@@ -14,15 +14,10 @@ int ReadPositiveNumber(string Message)
 	return clsInputValidate<int>::ReadPositiveNumber(); //1234
 }
 
-int ReverseNumber(int Number)
-{
-	return clsUtil<int>::ReverseNumber(Number);
-}
-
 int main()
 {
 	int Number = ReadPositiveNumber<int>("\nPlease Enter a Positive Number?");
-	cout << "Reversed Number is : " << ReverseNumber(Number) << endl;
+	cout << "Reversed Number is : " << clsUtil<int>::ReverseNumber(Number) << endl;
 
 	system("pause>0");
 	return 0;
