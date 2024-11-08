b7d91dbcb48ed82b061f2f023956c94f28bf72c0
Hanae Messari Khali <hanaae.messari@gmail.com>
2024-11-08 23:41:23 +0100

    Add files via upload

---
+++ Level 1/PrintName.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void PrintName(string Name)
+{
+	cout << "Your name is : " << Name << endl;
+}
+int main()
+{
+
+	PrintName("Hanae");
+
+	return 0;
+}
